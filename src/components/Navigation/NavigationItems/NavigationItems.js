e38f1f81cb41eb29abd5a21ac565cabb1c838981
Iiro <niemi.iiro@gmail.com>
2019-08-31 12:11:11 +0300

    Added Navigation items list

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import NavigationItem from './NavigationItem/NavigationItem';
+
+const navigationItems = (props) => (
+	<ul>
+		<NavigationItem />
+	</ul>
+);
+
+export default navigationItems; 
