b4a28b32d975bbd3cdbe76e26e24ea8b2e8e6c6b
Iiro <niemi.iiro@gmail.com>
2019-10-20 13:08:36 +0300

    178. Adding sidedrawer toggle button.

---
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Logo from '../../../Logo/Logo';
+
+const DrawerToggle = (props) => (
+		<div onClick={props.clicked}>
+			<div>MENU</div>
+		</div>
+)
+
+export default DrawerToggle;
